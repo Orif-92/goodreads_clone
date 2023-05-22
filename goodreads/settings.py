2bb19ec3d12bf26fbd66579acd92b42923675d2d
Orif-92 <sarniyozovorif@gmail.com>
2023-05-22 22:43:33 +0300

    DRF tests and pagination

--- goodreads/settings.py
+++ goodreads/settings.py
@@ -146,3 +146,8 @@ EMAIL_HOST_PASSWORD = 'qwerty'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 2
+}
